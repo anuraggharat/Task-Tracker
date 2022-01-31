5efd86a961e0d362b40e10f276ded367e0448317
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2022-01-31 22:15:01 +0530

    all apis done

--- components/Card.js
+++ components/Card.js
@@ -1,29 +1,35 @@
 import React from "react";
 import Link from 'next/link'
 
-export default function Card({task}) {
+export default function Card({data}) {
+
+  const due =new Date(data.due)
+  console.log(data)
   return (
     <Link href={`/task`}>
       <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-gray-100 my-3">
         <div className="px-3 pt-3 pb-1">
           <div className="font-bold text-xl mb-1 flex flex-row items-center text-grey-500">
-            The Coldest Sunset
+            {data.name}
           </div>
           <span className="inline-block mt-0 bg-red-200 rounded px-2 py-1 text-sm">
-            Freelancing
+            {data.category}
           </span>
         </div>
         <div className="grid grid-cols-2 mb-3 mt-2">
           <div className="px-3 pt-2 ">
             <h5 className="text-slate-500">Priority</h5>
             <span className=" inline-block mt-2 bg-red-200 rounded px-2 py-1 text-sm">
-              High
+              {data.priority}
             </span>
           </div>
           <div className="px-3 pt-2 ">
             <h5 className="text-slate-500">Due</h5>
             <span className="inline-block mt-2 bg-orange-200 rounded px-2 py-1 text-sm">
-              Sunday
+              {due.getDate() +
+                " " +
+                due.toLocaleString("en-us", { month: "short" }) +
+                " "}
             </span>
           </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,60 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header';
 import Card from "../components/Card";
+import axios from 'axios';
 
 
 
-export default function Home() {
+export default function Home(props) {
+
+
+  const {tasks} = props.pageProps
+
+      const todos = tasks.filter((item) => (item.status == "Todo"));
+      const doing = tasks.filter((item) => (item.status == "Doing"));
+      const done = tasks.filter((item) => (item.status == "Done"));
+
+      //console.log("todos",todos);
+       console.log("doing",doing);
+      // console.log("done",done);
+
+
+
+
+
   return (
     <div className="w-100 ">
       <Header />
       <div className="grid grid-cols-3 gap-8 mt-10">
         <div className="">
           <h3>Todo</h3>
-          <Card />
-          <Card />
-
-          <Card />
+          {todos.map((todo, index) => (
+            <Card data={todo} key={index} />
+          ))}
         </div>
         <div className="">
           <h3>In Progress</h3>
-          <Card />
-
-          <Card />
+          {doing.map((doing, index) => (
+            <Card data={doing}
+             key={index} />
+          ))}
         </div>
 
         <div className="">
           <h3>Completed</h3>
-          <Card />
+          {done.map((done, index) => (
+            <Card data={done} key={index} />
+          ))}
         </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const res = await axios("http://localhost:3000/api/task");
+  const {tasks } = res.data;
+  return {
+    props: { tasks: tasks },
+  };
 }
